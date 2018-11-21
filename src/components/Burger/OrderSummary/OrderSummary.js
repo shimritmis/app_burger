efde8e1b0c962c5929de2ed77f20102e28e9de9f
Shimrit Misgav <Shimritmis@gmail.com>
2018-11-21 18:14:17 +0200

    j Please enter the commit message for your changes. Lines starting orderSummary, modal, etc

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Auxiliary from '../../../hoc/Auxiliary';
+import Button from '../../UI/Button/Button';
+
+const orderSummary = (props) => {
+    const ingredientsSummary =Object.keys(props.ingredients)
+        .map (igKey => {
+            return <li key = {igKey}>
+                <span style={{texttransform:'capitalize'}}>{igKey}</span> : {props.ingredients[igKey]}</li>
+        });
+    return (
+        <Auxiliary>
+            <p><strong>Total Price: {props.price.toFixed(2)} </strong></p>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientsSummary}
+            </ul>
+            <p>Continue to checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+        </Auxiliary>
+
+    );
+};
+
+export default orderSummary;
