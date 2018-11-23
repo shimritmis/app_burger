2534f1d72d1599cb9c724be24928344c2cffcc40
Shimrit Misgav <Shimritmis@gmail.com>
2018-11-23 20:06:06 +0200

    new folder structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import Auxiliary from '../../../hoc/Auxiliary';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
+    //This could be a functional component, doesn't have to be a class!
         componentWillUpdate (){
             console.log('[OrderSummary] willUpdate');
         }
