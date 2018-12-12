9788ebda4d87f188b7382b61b5e84a83179feef0
Shimrit Misgav <Shimritmis@gmail.com>
2018-12-12 16:09:36 +0200

    more user feedback

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,13 +22,18 @@ class BurgerBuilder extends Component {
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
     }
     
     componentDidMount () {
         axios.get('https://burger-app-c78f6.firebaseio.com/ingredients.json')
         .then (response => {
             this.setState({ingredients: response.data});
+            })
+                        
+            .catch(error => {
+                this.setState({error: true})
         });
     }
     updatePurchaseState(ingredients) {
@@ -118,7 +123,7 @@ class BurgerBuilder extends Component {
         }
         let orderSummary = null; 
 
-        let burger = <Spinner /> 
+        let burger = this.state.error ? <p>Ingredients can't be loaded </p> : <Spinner /> 
         if (this.state.ingredients) {
             burger = (
                 <Auxiliary>
